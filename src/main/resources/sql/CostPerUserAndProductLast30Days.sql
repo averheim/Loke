11721bee51fe0de82ec81ee1636feb4416923d43
chris015 <christopher.olsson@hyperisland.se>
2017-10-13 11:28:19 +0200

    Added sql

---
+++ src/main/resources/sql/CostPerUserAndProductLast30Days.sql
@@ -0,0 +1,13 @@
+-- Cost / user / product / last 30 days
+SELECT
+  user_owner,
+  product_name,
+  sum(cast(blended_cost AS DOUBLE))                         AS cost,
+  cast(date_parse(usage_start_date, '%Y-%m-%d %T') AS DATE) AS start_date
+FROM wsbillingreports.report_item
+WHERE blended_cost != 'BlendedCost'
+      AND usage_start_date != ''
+      AND user_owner LIKE '%.%'
+      AND cast(date_parse(usage_start_date, '%Y-%m-%d %T') AS DATE) > (current_date - INTERVAL '30' DAY)
+GROUP BY user_owner, product_name, cast(date_parse(usage_start_date, '%Y-%m-%d %T') AS DATE)
+ORDER BY user_owner, start_date DESC;
