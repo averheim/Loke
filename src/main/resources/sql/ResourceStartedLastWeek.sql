ea5425d020741064ff2e2811fa924e20e6c614e8
chris015 <christopher.olsson@hyperisland.se>
2017-10-20 13:29:04 +0200

    fixed sql

--- src/main/resources/sql/ResourceStartedLastWeek.sql
+++ src/main/resources/sql/ResourceStartedLastWeek.sql
@@ -1,23 +1,23 @@
 -- User / Resource / Started last week
-
-SELECT DISTINCT
+SELECT
   linked_account_id,
   user_owner,
   product_name,
   resource_id,
-  MIN(usage_start_date),
+  MIN(usage_start_date)             AS start_date,
   sum(cast(blended_cost AS DOUBLE)) AS cost
 FROM wsbillingreports.report_item
 WHERE blended_cost != 'BlendedCost'
       AND usage_start_date != ''
+      AND usage_start_date != 'UsageStartDate'
       AND user_owner LIKE '%.%'
       AND resource_id NOT IN
           (
             SELECT DISTINCT resource_id
             FROM wsbillingreports.report_item
-            WHERE cast(date_parse(usage_start_date, '%Y-%m-%d %T') AS DATE) < (current_date - INTERVAL '20' DAY)
-                  AND usage_start_date != ''
+            WHERE usage_start_date != ''
                   AND usage_start_date != 'UsageStartDate'
+                  AND CAST(date_parse(usage_start_date, '%Y-%m-%d %T') AS DATE) < (CURRENT_DATE - INTERVAL '20' DAY)
           )
 GROUP BY linked_account_id, user_owner, product_name, resource_id
-ORDER BY user_owner, linked_account_id, product_name, resource_id ASC;_start_date ASC;start_date ASC;
+ORDER BY user_owner, linked_account_id, product_name, resource_id ASC;
