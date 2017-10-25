40af0ecaa8a51bef4c2ce62a14efb78c906a7181
chris015 <christopher.olsson@hyperisland.se>
2017-10-25 10:33:48 +0200

    Thousand Separator in DecimalFormatter

--- src/main/java/loke/utils/DecimalFormatter.java
+++ src/main/java/loke/utils/DecimalFormatter.java
@@ -2,17 +2,26 @@ package loke.utils;
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class DecimalFormatter {
 
     public static String format(double aDouble, int decimals) {
-        StringBuilder pattern = new StringBuilder("#.");
+        StringBuilder pattern = new StringBuilder("###,###.");
         for (int i = 0; i < decimals; i++) {
             pattern.append("#");
         }
-        DecimalFormat decimalFormat = new DecimalFormat(pattern.toString());
-        decimalFormat.setRoundingMode(RoundingMode.CEILING);
-        return decimalFormat.format(aDouble).replace(',', '.');
+
+        DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
+        formatter.applyPattern(pattern.toString());
+        formatter.setRoundingMode(RoundingMode.CEILING);
+        DecimalFormatSymbols symbols = formatter.getDecimalFormatSymbols();
+
+        symbols.setGroupingSeparator(' ');
+        formatter.setDecimalFormatSymbols(symbols);
+
+        return formatter.format(aDouble);
     }
 }
-
