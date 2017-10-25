e38580a27fe37d1f04b69e8459f5f4140e1dcb34
chris015 <christopher.olsson@hyperisland.se>
2017-10-25 12:30:57 +0200

    Fixed Resource Last Week sql

--- src/main/resources/sql/ResourceStartedLastWeek.sql
+++ src/main/resources/sql/ResourceStartedLastWeek.sql
@@ -1,22 +1,25 @@
 -- User / Resource / Started last week
 SELECT
-  linked_account_id AS account_id,
+  account.name                       AS account_name,
   user_owner,
   product_name,
   resource_id,
   MIN(usage_start_date)             AS start_date,
   sum(cast(blended_cost AS DOUBLE)) AS cost
 FROM wsbillingreports.report_item
+  INNER JOIN wsbillingreports.account
+    ON wsbillingreports.report_item.linked_account_id = wsbillingreports.account.id
 WHERE blended_cost != 'BlendedCost'
-      AND usage_start_date != ''
-      AND usage_start_date != 'UsageStartDate'
+      AND blended_cost != 'blended_cost'
+      AND user_owner != ''
       AND resource_id NOT IN
           (
             SELECT DISTINCT resource_id
             FROM wsbillingreports.report_item
             WHERE usage_start_date != ''
                   AND usage_start_date != 'UsageStartDate'
-                  AND CAST(date_parse(usage_start_date, '%Y-%m-%d %T') AS DATE) < (CURRENT_DATE - INTERVAL '20' DAY)
+                  AND usage_start_date != 'usage_start_date'
+                  AND CAST(date_parse(usage_start_date, '%Y-%m-%d %T') AS DATE) < (CURRENT_DATE - INTERVAL '7' DAY)
           )
-GROUP BY linked_account_id, user_owner, product_name, resource_id
-ORDER BY user_owner, linked_account_id, product_name, resource_id ASC;
+GROUP BY account.name, user_owner, product_name, resource_id
+ORDER BY account.name, user_owner, product_name, resource_id ASC;
